a51571e14bc5ba143de0415c2b68615e99a7fa38
Swaani Han <swaani.han@samsung.com>
2019-02-25 20:40:39 +0900

    Updates

---
+++ precache-manifest.58a80c28e8d670cb52e2e8a198a22cc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3d07953dfb554cfb69",
+    "url": "/movie-app/static/js/runtime~main.6e3d0795.js"
+  },
+  {
+    "revision": "ed29084caedc29ad7231",
+    "url": "/movie-app/static/js/main.ed29084c.chunk.js"
+  },
+  {
+    "revision": "e2a4fca7c1285f971d4a",
+    "url": "/movie-app/static/js/2.e2a4fca7.chunk.js"
+  },
+  {
+    "revision": "ed29084caedc29ad7231",
+    "url": "/movie-app/static/css/main.9dc45464.chunk.css"
+  },
+  {
+    "revision": "995655712ce854a0812752a67f8eea0d",
+    "url": "/movie-app/index.html"
+  }
+];
